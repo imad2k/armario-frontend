db15eb49cdb622d7c1af4a29696a1c15a433492d
Imad <iabusam@gmail.com>
2020-05-09 19:23:17 -0400

    landing page clean up

--- src/Components/LandingNav.js
+++ src/Components/LandingNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../design-assets/armarioLogo.svg';
+import LogoName from '../design-assets/armarioName.svg';
 
 
 
@@ -11,13 +11,13 @@ export default function LandingNav() {
     return (
         <div>
         <>
-            <div className='navBar'>
+            <div className='landingNav'>
                 <div className='logoNameWrapper'>
-                   <img className='navLogo' src={Logo} alt='app logo'/>
+                   <img className='landingLogo' src={LogoName} alt='app logo'/>
                 </div>
                 <div className='pageLinksWrapper'>
-                    <Link to='/login' className='navLink'>Login</Link>
-                    <Link to='/signup' className='navLink'>Sign Up</Link>
+                    <Link to='/login' className='loginLink'>Login</Link>
+                    <Link to='/signup' className='signupLink'>Sign Up</Link>
                 </div>
             </div>
         </>
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import LandingNav from './LandingNav';
 import LandingRouter from './LandingRouter';
+import { Link } from 'react-router-dom';
+import SectionImg from '../design-assets/landingPageImg.svg';
 
 
 
@@ -10,24 +12,35 @@ export default function LandingPage() {
     
 
     return (
-        <div>
+        
         <>
            <div>
             <LandingRouter />
             <LandingNav />
-        
-        
-        
-            <br />
-            <br />
-            <br />
-            <h1>This is the landing page</h1>
+
+            <div>
+                <section className='landingSection1'>
+                    <div className='leftText'>
+                        <p className='sec1line1'>Discover. Create. Save.</p>
+                        <p className='sec1para1'>Armario is a new type of shopping experience. Easily curate your outfits from items you already own and new pieces from your favorite brands.</p>
+                        
+                       
+                            
+                        <Link to='/signup' className='signupLink2'>Sign Up</Link>
+                        
+                    </div>
+                    
+                    <div className='rightImg'>
+                        <img src={SectionImg} alt='first section image' className='firstImg'></img>
+                    </div>
+                </section>
+            </div>
            
                 
         
                 
             </div>
         </>
-        </div>
+        
     )
 }
